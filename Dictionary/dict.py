37c024119fe5535bf389216cfa900dfa7fc7b594
shubhamsawant1 <shubhamsawant22011@gmail.com>
2026-06-30 14:22:49 +0530

    Dictionary operaction

---
+++ Dictionary/dict.py
@@ -0,0 +1,10 @@
+print("=" * 60)
+# EXERCISE 4: Advanced Dictionaries (15 min)
+print("=" * 60) 
+
+print("\n--- EXERCISE 4: Dictionary Operaction ---\n ")
+
+# Task 4.1: Dictionary methods 
+print("Task 4.1: Dictionary Methods")
+student = {"name": "siraj", "age": 21, "grade": "A"}
+print(f"Original: {student}")
--- day2.py
+++ day2.py
@@ -135,3 +135,66 @@ for fruit in fruits:
 
 fruits.append("grape")
 print(f"After append: {fruits}")
+
+fruits.insert(1, "blueberry")# Insert at position 1
+print(f"After insert: {fruits}")
+
+fruits.remove("Orange")
+print(f"After remove {fruits}")
+
+popped = fruits.pop() # remove and return last item
+print(f"After pop (removed {popped}):{fruits}")
+
+#Task 3.2: List sorting 
+print("n\
+      Task 3.2: Sorting")
+numbers = [5,6,4,6,9,4]
+print(f"Original: {numbers}")
+
+sorted_numbers = sorted(numbers) #Returns new sorted list
+print(f"Sorted (new list): {sorted_numbers}")
+
+numbers.sort() #Sorts the list in place
+print(f"After .sort(): {numbers}")
+
+# Task 3.3: Reversing 
+print("\nTask 3.3: Reversing")
+words = ["hello", "world", "python"]
+print(f"Original: {words}")
+words.reverse()
+print(f"Reversed: {words}")
+
+# Task 3.4: Checking if item exits
+print("\nTask 3.4: Membership testing")
+shopping_list = ["milk", "eggs", "bread", "cheese"]
+item_to_check = "eggs"
+
+if item_to_check in shopping_list:
+   print(f"{item_to_check} is in the list")
+else:
+   print(f"{item_to_check} is NOT in the list")
+
+# Task 3.5: List comprehension (create lists quickly)
+print("\nTask 3.5: List comprehension ")
+# old way: 
+squares = []
+for i in range(1, 6):
+   squares.append(i ** 2)
+print(f" Squares (new way): {squares}")
+
+# Filter even numbers
+numbers = [1,2,3,4,5,6,7,8,9,10]
+evens = [n for n in numbers if n % 2 == 0]
+print(f"Even numbers: {evens}")
+
+print("\nCHECKPOINT: Can you use list methods and comprehension?")
+
+print("=" * 60)
+# EXERCISE 4: Advanced Dictionaries (15 min)
+print("=" * 60) 
+
+print("\n--- EXERCISE 4: Dictionary Operaction ---\n ")
+# Task 4.1: Dictionary methods 
+print("Task 4.1: Dictionary Methods")
+student = {"name": "siraj", "age": 21, "grade": "A"}
+print(f"Original: {student}")
